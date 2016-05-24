fd20c6140be52e2bc552f4543acfd7cd8836a669
Jack Vine <jackv949@gmail.com>
2016-05-25 09:13:27 +0930

    Vector2 basics added.

--- MathForGames/MathLib/Vector2.cpp
+++ MathForGames/MathLib/Vector2.cpp
@@ -1,5 +1,6 @@
 #include "Vector2.h"
 
+//Constructors
 Vector2::Vector2()
 {
 	x = 0;
@@ -11,9 +12,10 @@ Vector2::Vector2(float xVal, float yVal)
 	x = xVal;
 	y = yVal;
 }
-
+//Deconstructor
 Vector2::~Vector2()
 {
+
 }
 
 
@@ -34,4 +36,14 @@ Vector2 Vector2::operator * (const float& other)
 {
 	x *= other;
 	y *= other;
+}
+
+Vector2::operator float*()
+{
+	float* array2 = new float[2];
+
+	array2[0] = x;
+	array2[1] = y;
+
+	return array2;
 }
--- MathForGames/MathLib/Vector2.h
+++ MathForGames/MathLib/Vector2.h
@@ -1,16 +1,19 @@
 #pragma once
+
 class Vector2
 {
 public:
 	//Constructors
 	Vector2();
 	Vector2(float xVal, float yVal);
+	//Deconstructor
 	~Vector2();
 
 	//Operator overloads
 	Vector2 operator + (const Vector2& other);
 	Vector2 operator - (const Vector2& other);
 	Vector2 operator * (const float& other);
+	operator float*();
 
 	//Public variable
 	float x = 0;
